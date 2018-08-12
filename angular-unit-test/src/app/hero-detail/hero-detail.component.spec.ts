e22488a2a346f8ebf7a26c4597a171a54f6a7627
Eleonora Dimchevska <e.dimchevska@gmail.com>
2018-08-12 16:47:18 +0300

    test for async calls

--- angular-unit-test/src/app/hero-detail/hero-detail.component.spec.ts
+++ angular-unit-test/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute } from '@angular/router';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, tick, async, flush } from '@angular/core/testing';
 import { Location } from '@angular/common';
 import { of } from 'rxjs/observable/of';
 import { FormsModule } from '@angular/forms';
@@ -37,12 +37,47 @@ describe('HeroesComponent (deep test)', () => {
 
         fixture = TestBed.createComponent(HeroDetailComponent);
 
+        // move this in before each because in first IT statement for some reason after added other it blocks this break the tests.
+        mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'SuperDude', strenght: 100 }));
     });
 
     it('should render hero name in a h2 tag', () => {
-        mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'SuperDude', strenght: 100 }));
         fixture.detectChanges();
 
         expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
     });
+
+
+    // test async code 1
+    // this work with both implementation of save method
+    it('should call update when save is called', fakeAsync(() => {
+        mockHeroService.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+        // comes from angular - this will make interval before continue (it can work without params)
+        // this is if we know how long to wait
+        // tick(250);
+
+        // if we dont know we can use :
+        flush();
+
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+    }));
+
+
+    // test async code 2
+    // this works only with second implementation of save method
+    it('should call update when save is called example 2', async(() => {
+        mockHeroService.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+
+        // this is related to how zone.js working
+        fixture.whenStable().then(() => {
+            expect(mockHeroService.updateHero).toHaveBeenCalled();
+        });
+
+    }));
 });
